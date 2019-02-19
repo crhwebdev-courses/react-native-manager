2fe6e4deaf3004c6b1b8fac90bd42a7611143934
Carl Hain <crhain@gmail.com>
2019-02-19 10:51:57 -0600

    Add firebase to project and initialize authentication service in App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import Config from '../.config';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: Config.API_KEY,
+      authDomain: Config.AUTH_DOMAIN,
+      databaseURL: Config.DATABASE_URL,
+      projectId: Config.PROJECT_ID,
+      storageBucket: Config.STORAGE_BUCKET,
+      messagingSenderId: Config.MESSEGING_SENDER_ID
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore(() => null)}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>Hello</Text>
         </View>
