6534a845a0960aa4e3547eb61d1a807ac2d3efea
Carl Hain <crhain@gmail.com>
2019-02-20 12:09:51 -0600

    Add authentication logic to create new user if user does not exist and to return an error message if user fails to log in

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
+} from './types';
 import firebase from 'firebase';
 
 export const emailChanged = text => {
@@ -15,14 +20,30 @@ export const passwordChanged = text => {
   };
 };
 
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+};
+
+const loginUserFail = dispatch => {
+  dispatch({ type: LOGIN_USER_FAIL });
+};
+
 export const loginUser = ({ email, password }) => {
   //return a function that takes dispatch argument for use by Redux Thunk
   return dispatch => {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(error => {
+        console.log(error);
+        //try to create a new user account
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          //failed to login, so show login error message
+          .catch(() => loginUserFail(dispatch));
       });
   };
 };
