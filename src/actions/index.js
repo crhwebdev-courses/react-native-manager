acd4f6d5aa68325853ac420798c4c463c75f5cf4
Carl Hain <crhain@gmail.com>
2019-02-20 11:23:07 -0600

    Login button now updates user key on auth state object when LOGIN_USER_SUCCESS action is fired on successfull login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
 import firebase from 'firebase';
 
 export const emailChanged = text => {
@@ -16,8 +16,13 @@ export const passwordChanged = text => {
 };
 
 export const loginUser = ({ email, password }) => {
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(user => console.log(user));
+  //return a function that takes dispatch argument for use by Redux Thunk
+  return dispatch => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(user => {
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+      });
+  };
 };
