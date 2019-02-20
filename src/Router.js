c9a2afda6792da0c8ad1ca79d7ada9e5e262d886
Carl Hain <crhain@gmail.com>
2019-02-20 16:27:21 -0600

    Add dummy EmployeeCreate component and hook it up in Router

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import React from 'react';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -9,21 +10,27 @@ const RouterComponent = () => {
       <Scene key="root" hideNavBar>
         <Scene key="auth">
           <Scene
+            initial
             key="login"
             component={LoginForm}
             title="Please Login"
-            initial
           />
         </Scene>
         <Scene key="main">
           <Scene
+            initial
             key="employeeList"
             component={EmployeeList}
             title="Employees"
             rightTitle="Add"
             onRight={() => {
-              console.log('right!!!');
+              Actions.employeeCreate();
             }}
+          />
+          <Scene
+            key="employeeCreate"
+            component={EmployeeCreate}
+            title="Create Employee"
           />
         </Scene>
       </Scene>
