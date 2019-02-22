92bdc836868a685e63d362f3801c0790fe9ca7a8
Carl Hain <crhain@gmail.com>
2019-02-22 08:42:40 -0600

    Update EmployeeEdit component to correctly update form fields with employee information and save edits to database when 'save' button pressed

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -2,6 +2,8 @@ import firebase from 'firebase';
 import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE,
+  EMPLOYEE_SAVE,
+  EMPLOYEE_SAVE_SUCCESS,
   EMPLOYEES_FETCH_SUCCESS
 } from './types';
 import { Actions } from 'react-native-router-flux';
@@ -24,6 +26,21 @@ export const employeeCreate = ({ name, phone, shift }) => {
       .push({ name, phone, shift })
       .then(() => {
         dispatch({ type: EMPLOYEE_CREATE });
+        Actions.employeeList({ type: 'reset' });
+      });
+  };
+};
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+  const { currentUser } = firebase.auth();
+
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .set({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
         Actions.employeeList({ type: 'reset' });
       });
   };
--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,15 +1,34 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
 import { Card, CardSection, Button } from './common';
-import { employeeEdit } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 
 class EmployeeEdit extends Component {
+  componentWillMount() {
+    /*iterate over each item in employee object passed to form
+      by ListItem component that navigated to this form
+      and run employeeUpdate action to update model with correct
+      values so they are populated into the form
+    */
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value });
+    });
+  }
+
   onButtonPress() {
     const { name, phone, shift } = this.props;
+    console.log(name, phone, shift);
     //call action creator with default for shift if picker is on default value
-    this.props.employeeEdit({ name, phone, shift: shift || 'Monday' });
+    this.props.employeeSave({
+      name,
+      phone,
+      shift,
+      uid: this.props.employee.uid
+    });
   }
+
   render() {
     return (
       <Card>
@@ -22,4 +41,12 @@ class EmployeeEdit extends Component {
   }
 }
 
-export default connect()(EmployeeEdit);
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(
+  mapStateToProps,
+  { employeeUpdate, employeeSave }
+)(EmployeeEdit);
